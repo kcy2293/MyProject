b7e27938b06eec63aaed9390f5f50a1b391149c1
kcy2293 <kcy2293@nate.com>
2015-12-03 17:23:38 +0900

    step-4 order by

--- views/js/controllers.js
+++ views/js/controllers.js
@@ -1,14 +1,20 @@
 var app = angular.module("MyApp", ["ngMaterial"]);
-
 app.controller("MyCtrl", function($scope, $mdSidenav) {
-	$scope.toggleMenu = function() {
-		$mdSidenav('left').toggle();
+	$scope.toggleSidenav = function() {
+		$mdSidenav("left").toggle();
 	};
-
-	$scope.phones = [
-		{"name" : "Nexus S", "description" : "Fast just got faster with Nexus S."},
-		{"name" : "Motorola XOOM™ with Wi-Fi", "description" : "The Next, Next Generation tablet."},
-		{'name': 'MOTOROLA XOOM™', 'description': 'The Next, Next Generation tablet.'}
-	];
-
+	$scope.orderProp = "age";
+	$scope.phones = [{
+		"name" : "Nexus S",
+		"desc" : "Fast just got faster with Nexus S.",
+		"age": 2
+	},{
+		"name" : "Motorola XOOM™ with Wi-Fi",
+		"desc" : "The Next, Next Generation tablet.",
+		"age": 1
+	},{
+		"name" : "MOTOROLA XOOM™",
+		"desc" : "The Next, Next Generation tablet.",
+		"age": 3
+	}];
 });
